21736169f5f3377c0614758e369ec074689076dc
NakYoun Keum <goldnyou@gmail.com>
2017-09-26 15:32:57 +0900

    aaa

---
+++ src/java_network/URLConnectionDGIT.java
@@ -0,0 +1,40 @@
+package java_network;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class URLConnectionDGIT {
+
+	public static void main(String[] args) {
+		try {
+			URL dgit=new URL("http://dgit.or.kr/login.php");
+			URLConnection urlConnection=dgit.openConnection();
+			urlConnection.setDoOutput(true);
+			
+			OutputStreamWriter osw =new OutputStreamWriter(urlConnection.getOutputStream());
+			osw.write("user_id=goldny&userpasswd=nykeum");
+			osw.close();
+			
+			BufferedReader br=new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+			String line=null;
+			while((line=br.readLine())!=null){
+				System.out.println(line);
+			}
+			br.close();
+		} catch (MalformedURLException e) {
+			
+			e.printStackTrace();
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+
+
+	}
+
+}
